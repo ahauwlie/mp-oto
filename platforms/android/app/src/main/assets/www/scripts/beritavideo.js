480676d7440607c574fe154d0edf333629169a73
Git~ <555+git@admin.noreply.github.com>
2019-05-10 10:34:26 +0700

    upup

---
+++ platforms/android/app/src/main/assets/www/scripts/beritavideo.js
@@ -0,0 +1,79 @@
+$.getJSON('https://adm982.kabaroto.com/api/post?rpp=20&page=1&category=39', function(data) {
+        
+    for (var i = 0; i < data.data.length; i++) {
+        var my = data.data[i].cover,
+        replacement = '_160x160.';
+        var str = my.replace(/.([^.]*)$/, replacement + '$1');
+        data.data[i].coverlite = str;
+        var my1 = data.data[i].cover,
+        replacement = '_318x150.';
+        var str1 = my1.replace(/.([^.]*)$/, replacement + '$1');
+        data.data[i].coverlite1 = str1;
+
+        var category = data.data[i].id;
+        var cover = data.data[i].coverlite;
+        var covercard = data.data[i].coverlite1;
+        var date = data.data[i].published;
+        var title = data.data[i].title;
+        var user = data.data[i].user.name;
+        var userid = data.data[i].user.id;
+        var minides = data.data[i].seo_description;
+
+        var table1 = `
+            <div class="news-list-item no-border bottom-0">
+                <a href="nyoba_detail.html?id=`+category+`">
+                    <img src="` + cover + `">
+                    <strong>` + title + `</strong>
+                </a>
+                <span><i class="fas fa-clock"></i>` + date + `<a href="userposts.html?id=`+userid+`" class="color-highlight">` + user + `</a></span>
+            </div>
+        `;
+
+        $("#beritavideo").append(table1);
+    }
+});
+
+var page = 1;
+function loadmore1() {
+    appendData1();
+}
+
+function appendData1() {
+    page++;
+    console.log(page);
+    $.getJSON('https://adm982.kabaroto.com/api/post?rpp=20&category=39&page=' + page, function(data) {
+        for(var i=0;i<data.data.length;i++){
+            var my = data.data[i].cover,
+            replacement = '_160x160.';
+            var str = my.replace(/.([^.]*)$/, replacement + '$1');
+            data.data[i].coverlite = str;
+            var my1 = data.data[i].cover,
+            replacement = '_318x150.';
+            var str1 = my1.replace(/.([^.]*)$/, replacement + '$1');
+            data.data[i].coverlite1 = str1;
+
+            var category = data.data[i].id;
+            var cover = data.data[i].coverlite;
+            var covercard = data.data[i].coverlite1;
+            var date = data.data[i].published;
+            var title = data.data[i].title;
+            var user = data.data[i].user.name;
+            var userid = data.data[i].user.id;
+            var minides = data.data[i].seo_description;
+
+            // buat UI yang di tampilin ke html
+            var table1 = `
+                <div class="news-list-item no-border bottom-0">
+                    <a href="nyoba_detail.html?id=`+category+`">
+                        <img src="` + cover + `">
+                        <strong>` + title + `</strong>
+                    </a>
+                    <span><i class="fas fa-clock"></i>` + date + `<a href="userposts.html?id=`+userid+`" class="color-highlight">` + user + `</a></span>
+                </div>
+            `;
+
+            //panggil ke <div id="mydiv">
+            $("#beritavideo").append(table1);
+        };
+    });
+}
